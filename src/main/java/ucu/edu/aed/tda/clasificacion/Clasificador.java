01802b0d567bf8d256d9a8d820487630c348e0ee
maaxilopp <maximiliano.lopez@correo.ucu.edu.uy>
2026-06-23 00:04:53 -0300

    agrego javadoc de particion en quicksort

--- src/main/java/ucu/edu/aed/tda/clasificacion/Clasificador.java
+++ src/main/java/ucu/edu/aed/tda/clasificacion/Clasificador.java
@@ -36,6 +36,19 @@ public class Clasificador extends TClasificador{
         return (i + j) / 2;
     }
 
+    /**
+     * Dado un pivote definido por el metodo realizado anteriormente y una coleccion de datos,
+     * particiona el subarreglo [i..j] en dos partes; una con los elementos cuyos valores son menores al del pivote
+     * y otra con los elementos cuyos valores son mayores o iguales al pivote. El orden de ejecución es O(n), siendo n el
+     * tamaño del subarreglo (j - i + 1); ya que los punteros l y r recorren el rango en una
+     * sola dirección hasta cruzarse, comparando cada elemento con el pivote una sola vez.
+     * @param datos  el arreglo sobre el que se realiza la partición
+     * @param i indice inicial
+     * @param j indice final
+     * @param pivote  valor de referencia; los menores quedan a la izquierda y los mayores o iguales a la derecha
+     * @return el índice de la primera posición de la mitad derecha, o sea el punto de corte que separa los menores
+     * de los mayores o iguales al pivote.
+     */
     @Override
     protected int particion(int[] datos, int i, int j, int pivote) {
         int l = i;
