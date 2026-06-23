8008cdcf7d1b24398541eabbcd5860f6e7db801f
Trinidad Bunge <trinibunge@gmail.com>
2026-06-22 21:12:09 -0300

    Changes by maxim

--- src/main/java/ucu/edu/aed/tda/clasificacion/Clasificador.java
+++ src/main/java/ucu/edu/aed/tda/clasificacion/Clasificador.java
@@ -23,22 +23,30 @@ public class Clasificador extends TClasificador{
     }
 
     /**
-     * Obtiene el pivote para hacer quicksort; que es el elemento ubicado en la mitad
-     * de la coleccion de datos; es O(1) ya que solo hace una operación aritmética y
-     * un acceso a la coleccion
+     * Obtiene el pivote para hacer quicksort; devuelve el índice del elemento
+     * ubicado en la mitad del subarreglo. Es O(1) ya que solo hace una
+     * operación aritmética.
      * @param datos arreglo sobre el que se busca el pivote
      * @param i índice inicial
      * @param j índice final
-     * @return el pivote; el elemento en el centro de la coleccion
+     * @return el índice del elemento central del subarreglo
      */
     @Override
     protected int obtenerPivote(int[] datos, int i, int j) {
-        return datos[(i + j) / 2];
+        return (i + j) / 2;
     }
 
     @Override
     protected int particion(int[] datos, int i, int j, int pivote) {
-        return 0;
+        int l = i;
+        int r = j;
+        do {
+            intercambiar(datos, l, r);
+            while(datos[l] < pivote){l ++;}
+            while(datos[r] >= pivote){r --;}
+        }
+        while(l  <= r);
+        return l;
     }
 
     @Override
